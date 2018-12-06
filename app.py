06c5ed759ba84900c7f97feea8d622bc917cca11
Lonnie Hansen <lg3hansen@gmail.com>
2018-12-05 21:40:21 -0800

    Updating webpage

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ configure_uploads(app, photos)
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        label_image = 'python label_image.py --labels=retrained_labels.txt --graph=retrained.pb --image=static/img/' + filename
+        label_image = 'python3 label_image.py --labels=retrained_labels.txt --graph=retrained.pb --image=static/img/' + filename
         os.system(label_image)
         results_file = open("results.txt", "r")
         result = results_file.readline();
-        breed = result[:result.find("0")]
+        dogBreed = result[:result.find("0")]
+        confidence = float(result[result.find("0"):])
         os.remove('static/img/' + filename)
-        return "Your dog is a " + breed
+        if confidence >= 0.85:
+            return render_template('upload.html', breed=dogBreed)
+        else:
+            return render_template('upload.html', error='true')
     return render_template('upload.html')
 
 
